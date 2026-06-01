a778c156f3b35d7f0f2c41fdb2ea8e93b656f3ce
Rohan Jadhav <rohanjadhav41247@gmail.com>
2026-06-01 12:58:17 +0530

    Create AppTest with a sample passing test
    Added a sample test that always passes.

---
+++ src/test/java/com/rohan/AppTest.java
@@ -0,0 +1,11 @@
+package com.rohan;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true); // This test always passes
+    }
+}
